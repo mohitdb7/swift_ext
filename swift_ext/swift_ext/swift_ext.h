5ada62741038be3d25b0bc9e70b8bcff6e81db33
mohitdubey60 <mohitdubey60@gmail.com>
2024-01-29 10:34:14 +0530

    Project creation - Swift Ext

---
+++ swift_ext/swift_ext/swift_ext.h
@@ -0,0 +1,18 @@
+//
+//  swift_ext.h
+//  swift_ext
+//
+//  Created by mohit.dubey on 29/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for swift_ext.
+FOUNDATION_EXPORT double swift_extVersionNumber;
+
+//! Project version string for swift_ext.
+FOUNDATION_EXPORT const unsigned char swift_extVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_ext/PublicHeader.h>
+
+
---
+++ swift_ext/swift_ext_example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_ext_example
+//
+//  Created by mohit.dubey on 29/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
